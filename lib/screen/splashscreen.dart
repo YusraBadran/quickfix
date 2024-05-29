0542a455205dac3f5acb09ef51644d3000189cb0
YusraBadran <yusrabadran1225@gmail.com>
2024-05-29 13:21:27 +0300

    inti

--- lib/screen/splashscreen.dart
+++ lib/screen/splashscreen.dart
@@ -1,7 +1,9 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:quickfix/core/storage/storage.dart';
+
 import 'package:quickfix/screen/welcomescreen.dart';
 
 class splashScreen extends StatefulWidget {
@@ -11,28 +13,59 @@ class splashScreen extends StatefulWidget {
   State<splashScreen> createState() => _splashScreenState();
 }
 
-class _splashScreenState extends State<splashScreen> {
-  @override
+class _splashScreenState extends State<splashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation animation;
+
   void initState() {
-    // Storage().initHive();
-    // TODO: implement initState
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+    animation =
+        CurvedAnimation(parent: animationController, curve: Curves.easeInOut);
+    animation.addListener(() {
+      if (mounted) {
+        setState(() {});
+      }
+    });
+    animationController.forward();
+    Future.delayed(const Duration(seconds: 2))
+        .then((value) => Get.to(() => Welcomescreen()));
     super.initState();
-    Timer(
-        Duration(seconds: 3),
-        () => Navigator.pushReplacement(
-            context, MaterialPageRoute(builder: (context) => Welcomescreen())));
+  }
+
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
   }
 
   @override
+  @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Center(
-        child: Image.asset(
-          "assets/images/logo.png",
-          fit: BoxFit.fill,
-          width: 200,
-          height: 160,
-        ),
+      body: SizedBox(
+        height: size.height,
+        width: size.width,
+        child: Center(
+            child: Image.asset(
+          'assets/images/logo.png',
+          height: animation.value * 160,
+        )),
+      ),
+      bottomNavigationBar: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(bottom: 15),
+            child: Text(
+              "Made In Yemen",
+              style: TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ],
       ),
     );
   }
